2fa2b7b08db560dde73be14890996488102fd61c
Jin <jin.s@onther.io>
2019-06-11 15:16:59 +0900

    eccpow: header change for eccpow

--- eth/vm/forks/eccpow/__init__.py
+++ eth/vm/forks/eccpow/__init__.py
@@ -9,6 +9,7 @@ from eth.vm.state import BaseState  # noqa: F401
 from .blocks import ConstantinopleBlock
 from .state import ConstantinopleState
 
+from .headers import compute_ecc_difficulty
 
 class ConstantinopleVM(ByzantiumVM):
     # fork name
@@ -17,3 +18,7 @@ class ConstantinopleVM(ByzantiumVM):
     # classes
     block_class = ConstantinopleBlock  # type: Type[BaseBlock]
     _state_class = ConstantinopleState  # type: Type[BaseState]
+
+    # TODO add compute_difficulty
+    # Methods
+    compute_difficulty = staticmethod(compute_ecc_difficulty)
--- eth/vm/forks/eccpow/headers.py
+++ eth/vm/forks/eccpow/headers.py
@@ -21,7 +21,7 @@ from .constants import (
 )
 
 
-def compute_byzantium_difficulty(parent_header, timestamp):
+def compute_ecc_difficulty(parent_header, timestamp):
     """
     https://github.com/ethereum/EIPs/issues/100
     """
@@ -32,10 +32,11 @@ def compute_byzantium_difficulty(parent_header, timestamp):
     offset = parent_difficulty // DIFFICULTY_ADJUSTMENT_DENOMINATOR
 
     has_uncles = parent_header.uncles_hash != EMPTY_UNCLE_HASH
+    time_diff_level = (timestamp - parent_timestamp) // BYZANTIUM_DIFFICULTY_ADJUSTMENT_CUTOFF
     adj_factor = max(
         (
             (2 if has_uncles else 1) -
-            ((timestamp - parent_timestamp) // BYZANTIUM_DIFFICULTY_ADJUSTMENT_CUTOFF)
+            (time_diff_level)
         ),
         -99,
     )
@@ -50,10 +51,15 @@ def compute_byzantium_difficulty(parent_header, timestamp):
         ) // BOMB_EXPONENTIAL_PERIOD
     ) - BOMB_EXPONENTIAL_FREE_PERIODS
 
-    if num_bomb_periods >= 0:
-        return max(difficulty + 2**num_bomb_periods, DIFFICULTY_MINIMUM)
+    if time_diff_level < -1:
+        ecc_diff_level = -1
     else:
-        return difficulty
+        ecc_diff_level = time_diff_level
+
+    if num_bomb_periods >= 0:
+        return max(difficulty + 2**num_bomb_periods, DIFFICULTY_MINIMUM), ecc_diff_level
+    else:
+        return difficulty, ecc_diff_level
 
 
 def create_byzantium_header_from_parent(parent_header, **header_params):
